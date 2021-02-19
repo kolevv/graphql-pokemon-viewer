1ee4592509097dbc3f54b0c366e3420d57e63636
kolevv <me@vkolev.online>
2021-02-19 20:08:08 +0200

    Pokemon data displayed

--- pokemon-frontend/src/components/App.js
+++ pokemon-frontend/src/components/App.js
@@ -7,7 +7,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <poke-mon></poke-mon>
+                <poke-mon id="1"></poke-mon>
             </header>
         </div>
     )
--- pokemon-frontend/src/components/Pokemon.js
+++ pokemon-frontend/src/components/Pokemon.js
@@ -1,42 +1,63 @@
 import { ApolloQuery, customElement, html } from '@apollo-elements/lit-apollo'
 import { gql, ApolloClient, InMemoryCache } from '@apollo/client/core'
-// import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
     uri: 'http://localhost:5000',
     cache: new InMemoryCache(),
 })
-class PokemonQueryElement extends ApolloQuery {
-    // query = gql`
-    //     query GetPokemon {
-    //         pokemon(name: "Bulbasaur") {
-    //             id
-    //             name
-    //         }
-    //     }
-    // `
-
-    variables = {
-        id: 'Howdy',
-        name: 'Partner',
+class Pokemon extends ApolloQuery {
+    constructor() {
+        super()
+        this.id = '1'
+        this.variables = {
+            id: btoa('Pokemon:' + String(this.id).padStart(3, '0')),
+        }
     }
+    static get properties() {
+        return {
+            id: { attribute: true, type: String },
+        }
+    }
+    variables = {
+        // id: btoa('Pokemon:' + String(this.id).padStart(3, '0')),
+        // id: 'Pokemon00' + this.getAttributeNames(),
+    }
+    query = gql`
+        query GetPokemon($id: String) {
+            pokemon(id: $id) {
+                image
+                name
+                number
+                attacks {
+                    fast {
+                        name
+                        type
+                        damage
+                    }
+                    special {
+                        name
+                        type
+                        damage
+                    }
+                }
+                resistant
+                weaknesses
+            }
+        }
+    `
 
     render() {
-        client
-            .query({
-                query: gql`
-                    query GetPokemon {
-                        pokemon(name: "Bulbasaur") {
-                            id
-                            image
-                        }
-                    }
-                `,
-            })
-            .then((result) => console.log(result))
-        const greeting = this.data?.hello?.greeting ?? 'hello'
-        const name = this.data?.hello?.name ?? 'world'
-        return html` <span id="hello"> ${greeting}, ${name}! </span> `
+        console.log(this.getAttribute('id'))
+        const name = this.data?.pokemon?.name ?? 'oops!'
+        const image = this.data?.pokemon?.image
+        const numericId = this.data?.pokemon.number ?? 'no!'
+        console.log(this.data?.pokemon.attacks)
+        return html`
+            <span id="hello"> ${name}</span>
+            <img src=${image} />
+            <span id="numeric id:"> ${numericId}</span>
+            <span id="attacks: "> ${this.data?.pokemon.resistant}</span>
+        `
     }
 }
-customElements.define('poke-mon', PokemonQueryElement)
+customElements.define('poke-mon', Pokemon)
